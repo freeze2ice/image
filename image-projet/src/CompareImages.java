626b5cc5b6433f894969995eddd8729b57a0c7a8
freeze2ice <freeze2ice@gmail.com>
2013-05-11 14:44:39 +0200

    CompareImages - calculateNote methode made, working on printToScreen(note)

--- image-projet/src/CompareImages.java
+++ image-projet/src/CompareImages.java
@@ -1,10 +1,28 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class CompareImages {
+
+public class CompareImages extends JPanel {
 	
 	public CompareImages(File image1, File image2) {
+		super(new BorderLayout());
+		
+		BufferedImage bi = null;
+		bi = printToScreen(toString(calculateNote(image1, image2)));
+		JLabel label = new JLabel();
+		label.setIcon(new ImageIcon(bi));
+		this.add(label, BorderLayout.NORTH);
+	}
+	
+	public static long calculateNote(File image1, File image2){
 		
 		Qcm qcm = new Qcm(image1);
 		Qcm2 qcm2 = new Qcm2(image2);
@@ -14,7 +32,7 @@ public class CompareImages {
 		
 		answers1 = qcm.getAnswers();
 		answers2 = qcm2.getAnswers();
-		int note=0;
+		long note=0;
 		for(int j=1; j<answers2.size(); j++){
 			if(answers2.get(j).equals(answers1.get(j))){
 				note++;
@@ -23,6 +41,24 @@ public class CompareImages {
 		System.out.println("******************");
 		System.out.println("**** note= " + note + " ****");
 		System.out.println("******************");
+		return note;
+	}
+	
+	public String toString(long note){
+		StringBuffer sb = new StringBuffer();
+		return sb.append(note).toString();
+	}
+	
+	public static BufferedImage printToScreen(String s){
+		BufferedImage output = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
+		for(int x=0; x<output.getWidth(); x++){
+			for(int y=0; y<output.getHeight(); y++){
+				output.setRGB(x, y, Color.WHITE.getRGB());
+			}
+		}
+		Graphics2D g = output.createGraphics();
+		g.drawString(s, 5, 5);
+		return output;
 	}
 
 }
--- image-projet/src/Hamed.java
+++ image-projet/src/Hamed.java
@@ -17,7 +17,7 @@ public class Hamed {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		File image = new File("qcms/qcmcolor.jpg");
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("\tInitial Image");
 		JPanel panel = new Qcm(image);
 		frame.getContentPane().add(panel, BorderLayout.NORTH);
 		frame.setLocation(50, 50);
@@ -26,14 +26,19 @@ public class Hamed {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		File image2 = new File("qcms/qcmcolor.jpg");
-		JFrame frame2 = new JFrame();
+		JFrame frame2 = new JFrame("\t2nd Image");
 		JPanel panel2 = new Qcm2(image2);
 		frame2.getContentPane().add(panel2, BorderLayout.NORTH);
 		frame2.pack();
 		frame2.setVisible(true);
 		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		CompareImages ci = new CompareImages(image, image2);
+		JPanel panel3 = new CompareImages(image, image2);
+		JFrame frame3 = new JFrame("\tResults");
+		frame3.getContentPane().add(panel3, BorderLayout.NORTH);
+		frame3.pack();
+		frame.setVisible(true);
+		frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
 
